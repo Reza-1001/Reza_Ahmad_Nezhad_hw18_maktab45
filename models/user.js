5cd1770ca04a0929c02a4a30b562a44cf4fc4da1
Reza Ahmad Nezhad <resistance1271@gmail.com>
2021-03-18 03:27:25 +0330

    .

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const bcrypt = require('bcrypt');
 const UserSchema = new Schema({
     username: {
         type: String,
@@ -22,4 +22,19 @@ const UserSchema = new Schema({
     }
 })
 
-module.exports = mongoose.model('user',UserSchema); 
+UserSchema.pre('save', function (next) {
+    const user = this;
+    if (user.isNew || user.isModified('password')) {
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) return next(err);
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                return next();
+            })
+        }) 
+    } else {
+        return next();
+    }
+})
+module.exports = mongoose.model('user', UserSchema);
